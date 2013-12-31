ed53a999a1ed132b31f8f4400d0521c0d1806e46
Josh Gitlin <joshua.gitlin@gmail.com>
2013-12-31 14:10:02 -0500

    first commit

---
+++ empsit.py
@@ -0,0 +1,29 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+def get_empsit_data():
+    empsit = urllib2.urlopen('http://www.bls.gov/news.release/empsit.a.htm')
+    print empsit.info()
+
+    soup = BeautifulSoup(empsit.read())
+    data_table = soup.find(id='cps_empsit_sum')
+
+    highlights = {'Participation rate':{},
+                  'Employment-population ratio':{},
+                  'Unemployment rate':{}}
+    for row in data_table.findAll('tr'):
+        if row.th and row.th.text in highlights:
+            cells = row.findAll('td')
+            if len(cells) == 5:
+                highlights[row.th.text] = {'year_ago' : cells[0].text,
+                        'two_months_ago' : cells[1].text,
+                        'last_month' : cells[2].text,
+                        'this_month' : cells[3].text,
+                        'change' : cells[4].text
+                }
+
+    empsit.close()
+
+    return highlights
+
+print get_empsit_data()
---
+++ reconobot.py
@@ -0,0 +1,15 @@
+import praw
+
+exit()
+
+r = praw.Reddit(user_agent='econobot')
+recon = r.get_subreddit('economics')
+submissions = recon.get_hot(limit=5);
+
+for x in submissions:
+    print str(x)
+
+
+#r.login("econobot", "buttered$noodles")
+#r.login("tootie", "22t00tie")
+#r.send_message("econobot", "hi", "yay")
